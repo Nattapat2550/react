d9574a214beef553f701737e7c8e7dedc1a3049c
Nattapat2550 <nyansungvon@gmail.com>
2025-09-22 19:06:08 +0700

    text

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/global.css';  // Add this
 import App from './App.js';
 import { ThemeProvider } from '@mui/material/styles';
-import theme from './theme';
+import theme from './theme.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
