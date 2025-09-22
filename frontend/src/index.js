2973f772fb8fbb74002e717e144e27026babebd2
Nattapat2550 <nyansungvon@gmail.com>
2025-09-22 19:04:11 +0700

    text

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/global.css';  // Add this
-import App from './App';
+import App from './App.js';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './theme';
 
